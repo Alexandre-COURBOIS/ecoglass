156836a2b139c3e02b6ff6f7242faa08f7c17f99
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2021-01-22 17:18:47 +0100

    Creating project structure : Pages && Services && Models // Creating some files into folders like models : Container && User // Creating some files into folders like Services : Containers && Auth-guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './Pages/home/home.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { MapComponent } from './Pages/map/map.component';
+import { RegisterComponent } from './Pages/register/register.component';
+import { UpdateUserComponent } from './Pages/update-user/update-user.component';
+import { UpdateUserPersonnalInformationsComponent } from './Pages/update-user/update-user-personnal-informations/update-user-personnal-informations.component';
+import { UpdateUserContactInformationsComponent } from './Pages/update-user/update-user-contact-informations/update-user-contact-informations.component';
+import { UpdateUserPasswordComponent } from './Pages/update-user/update-user-password/update-user-password.component';
+import { ForgotPasswordComponent } from './Pages/forgot-password/forgot-password.component';
+import { UserProfilComponent } from './Pages/user-profil/user-profil.component';
+import { NotFound404Component } from './Pages/not-found404/not-found404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    MapComponent,
+    RegisterComponent,
+    UpdateUserComponent,
+    UpdateUserPersonnalInformationsComponent,
+    UpdateUserContactInformationsComponent,
+    UpdateUserPasswordComponent,
+    ForgotPasswordComponent,
+    UserProfilComponent,
+    NotFound404Component
   ],
   imports: [
     BrowserModule,
@@ -15,4 +37,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
