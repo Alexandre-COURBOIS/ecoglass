f042ceffafe5a53ff5f8e1a5817722318438193f
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2021-02-09 23:17:06 +0100

    Creating a header and starting to design register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './Pages/home/home.component';
-import { LoginComponent } from './Pages/login/login.component';
-import { MapComponent } from './Pages/map/map.component';
-import { RegisterComponent } from './Pages/register/register.component';
-import { UpdateUserComponent } from './Pages/update-user/update-user.component';
-import { UpdateUserPersonnalInformationsComponent } from './Pages/update-user/update-user-personnal-informations/update-user-personnal-informations.component';
-import { UpdateUserContactInformationsComponent } from './Pages/update-user/update-user-contact-informations/update-user-contact-informations.component';
-import { UpdateUserPasswordComponent } from './Pages/update-user/update-user-password/update-user-password.component';
-import { ForgotPasswordComponent } from './Pages/forgot-password/forgot-password.component';
-import { UserProfilComponent } from './Pages/user-profil/user-profil.component';
-import { NotFound404Component } from './Pages/not-found404/not-found404.component';
+import {LoginComponent} from './Pages/login/login.component';
+import {MapComponent} from './Pages/map/map.component';
+import {RegisterComponent} from './Pages/register/register.component';
+import {UpdateUserComponent} from './Pages/update-user/update-user.component';
+import {UpdateUserPersonnalInformationsComponent} from './Pages/update-user/update-user-personnal-informations/update-user-personnal-informations.component';
+import {UpdateUserContactInformationsComponent} from './Pages/update-user/update-user-contact-informations/update-user-contact-informations.component';
+import {UpdateUserPasswordComponent} from './Pages/update-user/update-user-password/update-user-password.component';
+import {ForgotPasswordComponent} from './Pages/forgot-password/forgot-password.component';
+import {UserProfilComponent} from './Pages/user-profil/user-profil.component';
+import {NotFound404Component} from './Pages/not-found404/not-found404.component';
+import {HeaderComponent} from './Pages/header/header.component';
+import {FooterComponent} from './Pages/footer/footer.component';
+
 
 @NgModule({
   declarations: [
@@ -28,7 +31,9 @@ import { NotFound404Component } from './Pages/not-found404/not-found404.componen
     UpdateUserPasswordComponent,
     ForgotPasswordComponent,
     UserProfilComponent,
-    NotFound404Component
+    NotFound404Component,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
