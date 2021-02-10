b55d59b167daabd58e8127ebc394909fc3063a38
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2021-02-11 00:27:02 +0100

    Connexion is now avaible it returns well the jwt token

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -3,6 +3,7 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {ToastrService} from 'ngx-toastr';
 import {NgxUiLoaderService} from 'ngx-ui-loader';
 import {Router} from '@angular/router';
+import {AuthService} from '../../Services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +15,7 @@ export class LoginComponent implements OnInit {
   submitted = false;
   authForm = new FormGroup({email: new FormControl(), password: new FormControl()});
 
-  constructor(private formBuilder: FormBuilder, private router: Router, private toastr: ToastrService, private ngxService: NgxUiLoaderService) {
+  constructor(private formBuilder: FormBuilder, private router: Router, private authService: AuthService, private toastr: ToastrService, private ngxService: NgxUiLoaderService) {
   }
 
   ngOnInit() {
@@ -33,13 +34,41 @@ export class LoginComponent implements OnInit {
 
     if (this.authForm.valid) {
 
-      this.ngxService.startLoader('loader-01')
+      this.ngxService.startLoader('loader-01');
 
       const email = this.authForm.get('email')?.value;
       const password = this.authForm.get('password')?.value;
 
+      if (email && password) {
 
+        this.authService.login(email, password).subscribe(value => {
+          console.log(value);
 
+          this.ngxService.stopLoader('loader-01');
+
+          // stocker le jwt
+
+          // faire l'intercepteur
+
+          // Executer une requête pour récupérer les infos utilisateur
+
+          // Stocker en storage où cookie les infos utilisateur
+
+          // Rediriger vers le profil
+
+        }, error => {
+          if (error.error.message) {
+            this.toastr.error('Email ou mot de passe incorrect');
+          }
+          this.ngxService.stopLoader('loader-01');
+        });
+      } else {
+        this.toastr.error('Tiens ? Les informations renseignées ne sont pas correct !', 'Oups une erreur ?');
+        this.ngxService.stopLoader('loader-01');
+      }
+    } else {
+      this.toastr.error('Il semblerait que le formulaire n\'est pas renseigné correctement !', 'Oups une erreur ?');
+      this.ngxService.stopLoader('loader-01');
     }
   }
 
