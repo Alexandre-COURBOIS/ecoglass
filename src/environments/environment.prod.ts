0ab2235c7ef463c677b0daae11a1a24cd1844c21
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2021-03-04 18:05:25 +0100

    Adding mapbox key in prod environnement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://www.api.courboisalexandre.best/'
+  API_URL: 'https://www.api.courboisalexandre.best/',
+  mapBoxKey: 'pk.eyJ1Ijoib3dkYWQiLCJhIjoiY2tsMTI4Z2VpMGRrazJ1bzQwYTM2d3NhbiJ9.f7V1kPlLpPrQdIU-vXoiNA'
 };
