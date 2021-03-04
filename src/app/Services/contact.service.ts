e6339e123419f7165bd7e1fe297414aba3ba6820
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2021-03-04 13:11:39 +0100

    update on contact form

--- src/app/Services/contact.service.ts
+++ src/app/Services/contact.service.ts
@@ -10,6 +10,7 @@ export class ContactService {
   constructor(private httpClient: HttpClient) { }
 
   sendContactMessage(prenom: string, nom: string, email: string, message: string){
-    return this.httpClient.post(environment.API_URL + 'contact/send/message', {prenom: prenom, nom: nom, email: email, message: message});
+    return this.httpClient.post(environment.API_URL + 'contact/send/message', {nom: nom, prenom: prenom, email: email, message: message});
   }
+
 }
